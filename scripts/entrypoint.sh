382c92818065000246cad8a68f08e34b3f33a5ee
Roland T. Lichti <rlichti@kaiserpfalz-edv.de>
2025-06-19 08:31:54 +0200

    WIP: running container. Currently http does not give anything back ...

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env sh
 
+cp /etc/options.json /data/Config/options.json
+
 if [ -n "$FOUNDRY_ADMIN_KEY" ]; then
   echo $FOUNDRY_ADMIN_KEY | node /opt/foundry/encode_password.js > /data/Config/admin.txt
 fi
 
+if [ -n "$FOUNDRY_WORLD" ]; then
+  sed -e "s/\"world\":.*$/\"world\": \"$FOUNDRY_WORLD\",/" -i /data/Config/options.json
+fi
+
 cd /opt/foundry
 
-node main.js --dataPath=/data --adminKeyFile=/data/Config/admin.txt --skipWelcomeScreen --skipUpdateCheck
+rm -rf /data/Config/Options.json.lock || true
+node --inspect=0.0.0.0:9229 main.js --dataPath=/data --adminKeyFile=/data/Config/admin.txt --skipWelcomeScreen --skipUpdateCheck
